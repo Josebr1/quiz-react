9d453669e55c5f28472abd108b4b48a8b5728cfb
Jose Antonio <jose.antonio@7comm.com.br>
2018-07-20 15:46:06 -0300

    Finalizando front end com api criacao

--- src/actions/Subject.js
+++ src/actions/Subject.js
@@ -1,10 +1,24 @@
 import { createActions } from 'reduxsauce';
+import { createSubject } from './SubjectAPI';
 
-const { Types: Creators } = createActions({
+const { Types, Creators } = createActions({
     createSuccess: ['subject'],
     createError: ['error'],
     isLoading: ['loading']
 });
 
 export const SubjectTypes = Types;
+
 export default Creators;
+export function thunkCreateSubject(subject) {
+    return async (dispatch) => {
+        dispatch(Creators.isLoading(true));
+        try {
+            let res = await createSubject(subject);
+            dispatch(Creators.createSuccess(res.data));
+        } catch (err) {
+            dispatch(Creators.isLoading(false));
+            dispatch(Creators.createError(true));
+        }
+    }
+}
--- src/reducers/Subject.js
+++ src/reducers/Subject.js
@@ -1,17 +1,23 @@
 import { createReducer } from 'reduxsauce';
 import { SubjectTypes } from './../actions/Subject';
 
-export const INITIAL_STATE = { error: false, loading: false, subject : { name: '', description: ''}};
+export const INITIAL_STATE = { error: false, loading: false, subject: { name: '', description: '' } };
 
-export const createSuccess = (state = INITIAL_STATE, action) => {}
+export const createSuccess = (state = INITIAL_STATE, action) => { 
+    return {...state, error: false, loading: action.loading, subject: action.subject }
+}
 
-export const createError = (state = INITIAL_STATE, action) => {}
+export const createError = (state = INITIAL_STATE, action) => { 
+    return {...state, error: true, loading: action.loading, subject: action.subject }
+}
 
-export const isLoading = (state = INITIAL_STATE, action) => {}
+export const isLoading = (state = INITIAL_STATE, action) => { 
+    return {...state, error: false, loading: action.loading, subject: action.subject }
+}
 
 export const HANDLER = {
-    [SubjectTypes.CREATE_SUCCESS] : createSuccess,
-    [SubjectTypes.CREATE_ERROR] : createError,
-    [SubjectTypes.IS_LOADING] : isLoading,
+    [SubjectTypes.CREATE_SUCCESS]: createSuccess,
+    [SubjectTypes.CREATE_ERROR]: createError,
+    [SubjectTypes.IS_LOADING]: isLoading,
 }
 export default createReducer(INITIAL_STATE, HANDLER);
