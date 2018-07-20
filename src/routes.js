a953e77be0a3e166130d2b3061b48b430c035347
Jose Antonio <jose.antonio@7comm.com.br>
2018-07-20 11:25:00 -0300

    Iniciando tela de listagem de assunto

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+const routes = {
+  '/': 'Home',
+  '/dashboard': 'Dashboard',
+  '/subject/list': 'Listing Subject',
+
+};
+export default routes;
---
+++ src/views/Subject/SubjectList/SubjectList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class SubjectList extends Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <h1>SubjectList</h1>
+        )
+    }
+}
+
+export default SubjectList;
